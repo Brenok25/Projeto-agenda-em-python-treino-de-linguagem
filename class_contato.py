78473b5732b6ef7c996387d90dea06c71088d33c
Breno Kauan <bkauan395@gmail.com.br>
2022-06-27 08:46:49 -0300

    Criando objetos e suas variações

--- class_contato.py
+++ class_contato.py
@@ -1,1 +1,22 @@
-# Inicio de projeto 
+# class Nomeclass: # Cria uma nova classe contato - Usar maiscula 
+#     def __init__(self, atributo1, atributo2, atributo3): # Cria objetos na classe contato / Innit constroi [ um objeto - metodo ( argumentos ) ]
+#         self.cod = atributo1  # Cria os atributos d e contato -- self.code é um atributo do objeto
+#         self.tel = atributo2
+#         self.nome = atributo3
+#         pass
+
+
+
+class Contato: 
+    def __init__(self): 
+        # self.cod = cod  #input('Digite o seu Código') pode tbm
+        # self.tel = tel
+        # self.nome = nome
+
+        self.cod = input('Digite o seu Código')  #input('Digite o seu Código') pode tbm
+        self.tel = input('Digite o seu Telefone') # Com input em baixo n coloca argumentos em cima 
+        self.nome = input('Digite o seu Nome')
+
+        print('Hello World  //  Objeto criado Heeeee')
+
+        
---
+++ index.py
@@ -0,0 +1,5 @@
+from class_contato import *
+
+# contato = Contato('123','999564253', 'Breno')
+
+contato = Contato()
