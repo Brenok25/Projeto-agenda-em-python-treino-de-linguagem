b4d2aab8a9b97aa27e793df0c20de2e18132031c
Breno Kauan <bkauan395@gmail.com.br>
2022-07-06 09:12:50 -0300

    arquivo obsoleto - mantido pra estudo

--- class_menu.py
+++ class_menu.py
@@ -1,32 +1,32 @@
-from class_agenda import *
+# from class_agenda import *
 
-class Menu:
-    def __init__(self):
-        #Criar o objeto agenda -- Menu de navegação
-        agenda = Agenda()
+# class Menu:
+#     def __init__(self):
+#         #Criar o objeto agenda -- Menu de navegação
+#         agenda = Agenda()
 
-        while True: 
-            entrada = input(' 1 - Novo Cadastro\n 2 - Listar Usuarios Cadastrados\n 3 - Trocar Telefone\n 4 - Trocar Nome\n 5 - Excluir Contato\n 0 - Sair\n')
+#         while True: 
+#             entrada = input(' 1 - Novo Cadastro\n 2 - Listar Usuarios Cadastrados\n 3 - Trocar Telefone\n 4 - Trocar Nome\n 5 - Excluir Contato\n 0 - Sair\n')
 
-            if entrada == '1':
-                agenda.salvar_contato()
+#             if entrada == '1':
+#                 agenda.salvar_contato()
 
-            elif entrada == '2':
-                agenda.listar_contato()
+#             elif entrada == '2':
+#                 agenda.listar_contato()
 
-            elif entrada == '3':
-                agenda.trocar_tel()
+#             elif entrada == '3':
+#                 agenda.trocar_tel()
                 
-            elif entrada == '4':
-                agenda.trocar_mome()
+#             elif entrada == '4':
+#                 agenda.trocar_mome()
 
-            elif entrada == '5':
-                cod = input('Informe o código do contato: ')
-                agenda.exclui_contato(cod)
-                # Tirar todos os inputs das funções e colocar aqui 
+#             elif entrada == '5':
+#                 cod = input('Informe o código do contato: ')
+#                 agenda.exclui_contato(cod)
+#                 # Tirar todos os inputs das funções e colocar aqui 
             
-            elif entrada == '0':
-                print('Obrigado por usar nosso sistema')
-                break
-            else:
-                print('Opção invalida!!!')
+#             elif entrada == '0':
+#                 print('Obrigado por usar nosso sistema')
+#                 break
+#             else:
+#                 print('Opção invalida!!!')
